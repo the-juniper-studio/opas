36195e21aa397230056767045f748320be5334fb
Jodie doubleday <jodie.doubleday@gmail.com>
2019-07-12 15:37:08 +0100

    adding a provacy policy

--- src/pages/privacy.js
+++ src/pages/privacy.js
@@ -6,7 +6,7 @@ const PrivacyPage = props => (
   <Layout>
     <SEO title="Home" />
     <div className="row">
-      <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
+      <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2">
         <h1>Privacy Policy (GDPR)</h1>
         <p><small>Last updated: [<span contenteditable="true">date</span>]</small></p>
         
