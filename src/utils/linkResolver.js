b432f454630499fa09b151204c6afd527419383a
Jodie doubleday <jodie.doubleday@gmail.com>
2019-08-02 12:40:30 +0100

    trying to get previews to work

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,19 +1,7 @@
 exports.linkResolver = function linkResolver(doc) {
 // Link Resolver is the same as in any other Prismic project
-  if (doc.type === 'homepage') {
-    return '/';
-  }
-  if (doc.type === 'products') {
-    return '/products';
-  }
-  if (doc.type === 'product') {
-    return '/products/' + doc.uid;
-  }
-  if (doc.type === 'blog_home') {
-    return '/blog';
-  }
-  if (doc.type === 'blog_post') {
-    return '/blog/' + doc.uid;
+  if (doc.type === 'Landing_page') {
+    return '/article';
   }
   return '/';
 }
