b7b7195137af11ddf3ee65608b64ced37a7ba05d
Jodie doubleday <jodie.doubleday@gmail.com>
2019-08-06 22:13:49 +0100

    adding prismic to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,37 @@
 import React from "react"
 import Layout from "../components/layout"
+import { graphql } from 'gatsby'
 import SEO from "../components/seo"
 import Countdown from 'react-countdown-now';
+import { RichText } from 'prismic-reactjs';
+import { linkResolver } from '../utils/linkResolver';
+
+
+export const indexQuery = graphql`
+  query {
+    prismic {
+      allIndex_pages(uid:null) {
+        edges {
+          node {
+            _meta {
+              uid
+              tags
+            }
+            meta_title
+            meta_keywords
+            meta_description
+            title
+            content
+            countdown_deadline
+            countdown_title
+            countdown_text
+            hero
+          }
+        }
+      }
+    }
+  }
+`
 
 const renderer = ({ days, hours, minutes, seconds }) => {
   return (
@@ -14,124 +44,133 @@ const renderer = ({ days, hours, minutes, seconds }) => {
   )
 };
 
-
-const IndexPage = props => (
-  <Layout>
-    <SEO title="Homepage" />
-    <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
-      <ol className="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-        <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-      </ol>
-      <div className="carousel-inner" role="listbox">
-        <div className="item active">
-          <img src="https://images.unsplash.com/photo-1458413111252-87446cbff277?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2495&q=80" alt="..." />
+const RenderBody = ({ indexPage }) => {
+  return (
+    <Layout>
+      <SEO title="Homepage" />
+      <div id="carousel-example-generic" className="carousel slide" data-ride="carousel">
+        <ol className="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
+        </ol>
+        <div className="carousel-inner" role="listbox">
+          <div className="item active">
+            <img src={indexPage.hero.url} alt={indexPage.hero.alt} />
+          </div>
         </div>
-        <div className="item">
-          <img src="https://bx1d1websitecepprd.azureedge.net/sites/default/azbfiles/styles/xxl_9_4/public/2019-03/Global-Banner-Darker.jpg" alt="..." />
+
+        <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span className="sr-only">Previous</span>
+        </a>
+        <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span className="sr-only">Next</span>
+        </a>
+      </div>
+      <div className="container-fluid mt-2">
+        <div className="row">
+          <div className="col-xs-12 col-md-8">
+            <main className="main" id="main" role="main">
+              <h1>{indexPage.title}</h1>
+              <p>{RichText.render(indexPage.content, linkResolver)}</p>
+              <h2 className="text-center">Latest Properties</h2>
+              <div className='row'>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>               
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <h2 className="text-center">Recently Sold</h2>
+              <div className='row'>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>               
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+                <div className='col-xs-6 col-md-4'>
+                  <div className="thumbnail">
+                    <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
+                    <div className="caption">
+                      <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
+                      <div className="h2 mt-0">£800,000</div>
+                      <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
+                      <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </main>
+          </div>
+          <div className="col-xs-12 col-md-4">
+            <aside role="complementary">
+              <div className="well text-center">
+                <div>{RichText.render(indexPage.countdown_title, linkResolver)}</div>
+                <Countdown date={indexPage.countdown_deadline} zeroPadTime='2' zeroPadDays='2' renderer={renderer} />
+                <div>{RichText.render(indexPage.countdown_text, linkResolver)}</div>
+              </div>
+            </aside>
+          </div>
         </div>
       </div>
+    </Layout>
+  )
+}
 
-      <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-    <div className="container-fluid mt-2">
-      <div className="row">
-        <div className="col-xs-12 col-md-8">
-          <main className="main" id="main" role="main">
-            <h1>Buy or sell with Scottish Property Auctions</h1>
-            <p>Welcome to BIDLOGIX Auctions, Scotland's largest auction house network, offering the biggest volume and widest variety of stock and asset offerings to the secondary market.</p>
-            <h2 className="text-center">Latest Properties</h2>
-            <div className='row'>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>               
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <h2 className="text-center">Recently Sold</h2>
-            <div className='row'>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>               
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-              <div className='col-xs-6 col-md-4'>
-                <div className="thumbnail">
-                  <img src="https://media.rightmove.co.uk/dir/crop/10:9-16:9/88k/87209/68312137/87209_FPS1001256_IMG_01_0004_max_656x437.jpg" alt="..." />
-                  <div className="caption">
-                    <h3 className="h6 mb-0 text-muted">4 Bed Detatched House</h3>
-                    <div className="h2 mt-0">£800,000</div>
-                    <p className="text-muted">The Dalby, The Observatory, Canterbury</p>  
-                    <a href="#test" className="btn btn-default btn-sm btn-block" role="button">View</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </main>
-        </div>
-        <div className="col-xs-12 col-md-4">
-          <aside role="complementary">
-            <div className="well text-center">
-              <h2 className="mt-0">Next Auction</h2>
-              <Countdown date={Date.now() + 500000000} zeroPadTime='2' zeroPadDays='2' renderer={renderer} />
-            </div>
-          </aside>
-        </div>
-      </div>
-    </div>
-  </Layout>
-)
+const IndexPage = props => {
+  const doc = props.data.prismic.allIndex_pages.edges.slice(0,1).pop();
+  if(!doc) return null;
 
+  return(
+    <Layout>
+      <SEO title={doc.node.meta_title} keywords={doc.node.meta_keywords} description={doc.node.meta_description} lang='en-gb' />
+      <RenderBody indexPage={doc.node} />
+    </Layout>
+  )
+}
 export default IndexPage
