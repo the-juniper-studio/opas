978b916f2f5b741c82efb3379f5b58f7ed387c36
Jodie doubleday <jodie.doubleday@gmail.com>
2019-08-11 22:14:16 +0100

    adding bidlogix to all teh things

--- src/html.js
+++ src/html.js
@@ -13,6 +13,39 @@ export default function HTML(props) {
         />
         <script defer src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossOrigin="anonymous" />
         <script defer src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossOrigin="anonymous" />
+        <script defer src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Intl.~locale.en,Promise" />
+        <script defer src="https://maps.googleapis.com/maps/api/js?key=MY_GOOGLE_MAPS_API" type="text/javascript" />
+        <script defer src="https://hove.eu-west-2.bidjs.com/static/2.0/js/bid-dependencies.min.js" />
+        <script defer src="https://hove.eu-west-2.bidjs.com/static/2.0/prettyphoto/jquery.prettyPhoto.min.js" />
+        <script defer src="https://hove.eu-west-2.bidjs.com/static/2.0/js/hbs-bid.min.js" />
+        <script defer src="https://hove.eu-west-2.bidjs.com/static/2.0/js/bid.min.js" />
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
+
+        <script>{`
+          window.bidjs = {  
+            config: {    
+              apiBase: "https://hove.eu-west-2.bidjs.com/auction-007/api",
+              clientId: "/demonstration",
+              staticBase: "https://hove.eu-west-2.bidjs.com/static/2.0",
+              staticCDN: 'https://static.bidjs.com', // Do not change    
+              staticVersion: '1.5', // e.g. 1.3  
+              templateVariant: 'bootstrap3' // either 'bootstrap3' or 'bootstrap4'  
+              },  
+            modules: {    
+              auctionDetails: '#!/auctionDetails/%AUCTION_ID%',
+              auctionsArchived: true,
+              invoices: true,
+              lotDetails: '#!/itemDetails/%ITEM_ID%',
+              mySales: true
+              },  
+            options: {
+              allowTitleChange: true,
+              allowMetaDescriptionChange: true,
+              standalone: false,
+              unsupportedBrowserMessage: 'Your browser is not supported. Please use a browser such as Google Chrome or Microsoft Edge to ensure full functionality'
+            }
+          }
+        `}</script>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
--- src/pages/auction.js
+++ src/pages/auction.js
@@ -37,42 +37,6 @@ function scripting() {
 const IndexPage = () => {
   return (
     <Layout>
-      <Helmet>
-        <script src="https://cdn.polyfill.io/v2/polyfill.min.js?features=Intl.~locale.en,Promise" />
-        <script src="https://maps.googleapis.com/maps/api/js?key=MY_GOOGLE_MAPS_API" type="text/javascript" />
-        <script src="https://hove.eu-west-2.bidjs.com/static/2.0/js/bid-dependencies--no-bootstrap.min.js" />
-        <script src="https://hove.eu-west-2.bidjs.com/static/2.0/prettyphoto/jquery.prettyPhoto.min.js" />
-        <script src="https://hove.eu-west-2.bidjs.com/static/2.0/js/hbs-bid.min.js" />
-        <script src="https://hove.eu-west-2.bidjs.com/static/2.0/js/bid.min.js" />
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-
-        <script>{`
-          window.bidjs = {  
-            config: {    
-              apiBase: "https://hove.eu-west-2.bidjs.com/auction-007/api",
-              clientId: "/demonstration",
-              staticBase: "https://hove.eu-west-2.bidjs.com/static/2.0",
-              staticCDN: 'https://static.bidjs.com', // Do not change    
-              staticVersion: '1.5', // e.g. 1.3  
-              templateVariant: 'bootstrap3' // either 'bootstrap3' or 'bootstrap4'  
-              },  
-            modules: {    
-              auctionDetails: '#!/auctionDetails/%AUCTION_ID%',
-              auctionsArchived: true,
-              invoices: true,
-              lotDetails: '#!/itemDetails/%ITEM_ID%',
-              mySales: true
-              },  
-            options: {
-              allowTitleChange: true,
-              allowMetaDescriptionChange: true,
-              standalone: false,
-              unsupportedBrowserMessage: 'Your browser is not supported. Please use a browser such as Google Chrome or Microsoft Edge to ensure full functionality'
-            }
-          }
-        `}</script>
-      </Helmet>
-      
       <div className="container-fluid">
         <noscript id="deferred-styles">
           <link href="https://hove.eu-west-2.bidjs.com/static/2.0/prettyphoto/jquery.prettyPhoto.min.css" rel="stylesheet" type="text/css" />
@@ -81,7 +45,7 @@ const IndexPage = () => {
         <div id='bidlogix-app' className='bidlogix-app'>
           <div className='text-center'>
             <svg width='200px' height='200px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' className='lds-dual-ring'>
-              <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='20' strokeWidth='2' stroke='#3f4ea1' strokeDasharray='31.41592653589793 31.41592653589793' transform='rotate(150 50 50)'>
+              <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='20' strokeWidth='2' stroke='#337ab7' strokeDasharray='31.41592653589793 31.41592653589793' transform='rotate(150 50 50)'>
                 <animateTransform attributeName='transform' type='rotate' calcMode='linear' values='0 50 50;360 50 50' keyTimes='0;1' dur='3s' begin='0s' repeatCount='indefinite'></animateTransform>
               </circle>
               <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='15' strokeWidth='2' stroke='#333' strokeDasharray='23.561944901923447 23.561944901923447' strokeDashoffset='23.561944901923447' transform='rotate(-150 50 50)'>
@@ -93,7 +57,7 @@ const IndexPage = () => {
         <div className='bidlogix-app'>
           <div id='bidlogix-modal'></div>
         </div>
-        <div className='bidlogix-app mt-3'>
+        <div className='bidlogix-app'>
           <div id='bidjs'></div>
         </div>
         <div dangerouslySetInnerHTML={scripting()} />
