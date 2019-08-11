4934a877e24b6c7bc20e29c3f5bf8874dbdfc996
Jodie doubleday <jodie.doubleday@gmail.com>
2019-08-11 19:44:59 +0100

    adding margin top

--- src/pages/auction.js
+++ src/pages/auction.js
@@ -84,7 +84,7 @@ const IndexPage = () => {
         <div id='bidlogix-app' className='bidlogix-app'>
           <div className='text-center'>
             <svg width='200px' height='200px' xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100' preserveAspectRatio='xMidYMid' className='lds-dual-ring'>
-              <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='20' strokeWidth='2' stroke='#337ab7' strokeDasharray='31.41592653589793 31.41592653589793' transform='rotate(150 50 50)'>
+              <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='20' strokeWidth='2' stroke='#3f4ea1' strokeDasharray='31.41592653589793 31.41592653589793' transform='rotate(150 50 50)'>
                 <animateTransform attributeName='transform' type='rotate' calcMode='linear' values='0 50 50;360 50 50' keyTimes='0;1' dur='3s' begin='0s' repeatCount='indefinite'></animateTransform>
               </circle>
               <circle cx='50' cy='50' fill='none' strokeLinecap='round' r='15' strokeWidth='2' stroke='#333' strokeDasharray='23.561944901923447 23.561944901923447' strokeDashoffset='23.561944901923447' transform='rotate(-150 50 50)'>
@@ -96,7 +96,7 @@ const IndexPage = () => {
         <div className='bidlogix-app'>
           <div id='bidlogix-modal'></div>
         </div>
-        <div className='bidlogix-app'>
+        <div className='bidlogix-app mt-3'>
           <div id='bidjs'></div>
         </div>
         <div dangerouslySetInnerHTML={scripting()} />
