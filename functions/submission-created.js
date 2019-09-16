6fdd38a48b2d28b468683d5019f467a67f2f0178
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-16 23:12:12 +0100

    parsing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -4,20 +4,27 @@ const { CREDAS_TOKEN } = process.env
 
 exports.handler = async (event, context, callback) => {
   let payload = JSON.parse(event.body).payload;
-  console.log(`START`)
-  console.log(`payload ${payload}`)
-  console.log(`please - ${payload.data.forename}`)
-
   return fetch('https://pi-api.credas.co.uk/api/registrations', {
     method: 'POST',
     headers: {
-      apikey: `${CREDAS_TOKEN}`,
+      apikey: CREDAS_TOKEN,
     },
-    body: event.body
+    body: {
+      forename: payload.data.forename,
+      surname: payload.data.surname,
+      phoneNumber: payload.data.phoneNumber,
+      emailAddress: payload.data.emailAddress,
+      regTypeId: "Cc466776a-9663-4812-8312-c5f00aa90796",
+      referenceId: "HID/12345/336/10",
+      sendEmail: true,
+      diallingCode: "+44",
+      duplicateAcknowledgement: false,
+      provideWebJourneyLink: true,
+    }
   })
     .then(response => response.json())
     .then(data => {
-      console.log(`END`)
+      console.log(payload.data.regTypeId)
     })
     .catch(error => ({ statusCode: 422, body: String(error) }))
 }
