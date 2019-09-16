51ea70d7e758bfe03285cc8782c28315ddcbeeb6
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-16 22:57:22 +0100

    parsing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -2,10 +2,8 @@ require('dotenv').config()
 const fetch = require('node-fetch')
 const { CREDAS_TOKEN } = process.env
 
-exports.handler = async event => {
+exports.handler = async (event, context, callback) => {
   console.log(`START`)
-  console.log(`event body is killing me ${event.body}`)
-  console.log(`diallingCode iYES ${event.body.diallingCode}`)
 
   return fetch('https://pi-api.credas.co.uk/api/registrations', {
     method: 'POST',
