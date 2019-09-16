1dd85cf110ece1826833ffb340d2290c0e57a53a
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-16 22:40:45 +0100

    parsing

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,13 +3,9 @@ const fetch = require('node-fetch')
 const { CREDAS_TOKEN } = process.env
 
 exports.handler = async event => {
+  console.log(`START`)
   console.log(`event body is killing me ${event.body}`)
   console.log(`diallingCode is ${event.body.diallingCode}`)
-
-  const params = querystring.parse(event.body);
-  const name = params.diallingCode;
-  console.log(`Params ${params}`)
-  console.log(`Name ${name}`)
 
   return fetch('https://pi-api.credas.co.uk/api/registrations', {
     method: 'POST',
