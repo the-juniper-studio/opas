d0d9f5c4712e31cc551352d082277cc5b25e5395
Nicki Lever <nicki.lever@holidayextras.com>
2019-09-17 16:42:38 +0100

    mark chagnes

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import Ombudsman from "../images/the-property-ombudsman.png"
 import TSI from "../images/TSI.png"
+import RightMove from "../images/right-move.png"
 
 const Footer = () => (
   <footer className="footer mt-3" role="contentinfo">
@@ -15,7 +16,7 @@ const Footer = () => (
           G2 4JR<br />
           <p className=''>T: <a href="tel:+441412660125">0141 266 0125</a></p>
         </div>
-        <div className="col-xs-12 col-sm-4">
+        <div className="col-xs-12 col-sm-3">
           <h4>Links</h4>
           <ul className="list-unstyled">
             <li><Link to="/privacy-policy">Privacy Policy</Link></li>
@@ -23,13 +24,16 @@ const Footer = () => (
             <li><Link to="/contact">Contact</Link></li>
           </ul>
         </div>
-        <div className="col-xs-12 col-sm-4">
+        <div className="col-xs-12 col-sm-5">
           <div className="row d-flex">
-            <div className="col-xs-6">
+            <div className="col-xs-4">
               <img alt="The Property Ombudsman" src={Ombudsman} loading="lazy" className="img-responsive" />
             </div>
-            <div className="col-xs-6">
+            <div className="col-xs-4">
               <img alt="TSI" src={TSI} loading="lazy" className="img-responsive" />
+            </div>
+            <div className="col-xs-4">
+              <img alt="Right Move" src={RightMove} loading="lazy" className="img-responsive" />
             </div>
           </div>
         </div>
