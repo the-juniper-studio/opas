ea39f199f45a644139ae1346c0f42e73d546ed80
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-17 21:19:00 +0100

    a working id check page

--- src/pages/credas.js
+++ src/pages/credas.js
@@ -1,15 +1,40 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { graphql } from 'gatsby'
+import { RichText } from 'prismic-reactjs';
+import { linkResolver } from '../utils/linkResolver';
 
-const RenderBody = () => {
+export const IdCheckQuery = graphql`
+  query IdCheckPageQuery($uid: String) {
+    prismic {
+      allIdCheck_pages(uid: $uid) {
+        edges {
+          node {
+            _meta {
+              uid
+              tags
+            }
+            meta_title
+            meta_keywords
+            meta_description
+            title
+            content
+          }
+        }
+      }
+    }
+  }
+`
+
+const RenderBody = ({ IdCheckPage }) => {
   return (
     <React.Fragment>
-      <SEO title="Contact" />
       <div className="container-fluid">
         <div className="row">
           <div className="col-xs-12 col-sm-10 col-sm-offset-1 col-md-8 col-md-offset-2 col-lg-6 col-lg-offset-3">
-            <h1>Credas</h1>
+            <h1>{IdCheckPage.title}</h1>
+            {RichText.render(IdCheckPage.content, linkResolver)}
             <form
               name="Credas"
               method="POST"
@@ -27,6 +52,10 @@ const RenderBody = () => {
               <input type="hidden" name="provideWebJourneyLink" value="true" />
 
               <div className="form-group">
+                <label htmlFor="emailAddress">Email Address<sup>*</sup></label>
+                <input className="form-control" type="email" name="emailAddress" id="emailAddress" required />
+              </div>
+              <div className="form-group">
                 <label htmlFor="forename">First Name<sup>*</sup></label>
                 <input className="form-control" type="text" name="forename" id="forename" required />
               </div>
@@ -38,13 +67,8 @@ const RenderBody = () => {
                 <label htmlFor="phoneNumber">Phone Number<sup>*</sup></label>
                 <input className="form-control" type="tel" name="phoneNumber" id="phoneNumber" required />
               </div>
-
               <div className="form-group">
-                <label htmlFor="emailAddress">Email Address<sup>*</sup></label>
-                <input className="form-control" type="emailAddress" name="emailAddress" id="emailAddress" required />
-              </div>
-              <div className="form-group">
-                <button className="btn btn-primary" type="submit">Send</button>
+                <button className="btn btn-block btn-primary" type="submit">Send</button>
               </div>
             </form>
           </div>
@@ -54,11 +78,16 @@ const RenderBody = () => {
   )
 }
 
-const CredasPage = props => {
+const IdCheckPage = props => {
+  const doc = props.data.prismic.allIdCheck_pages.edges.slice(0,1).pop();
+  if(!doc) return null;
+
   return (
     <Layout>
-      <RenderBody />
+      <SEO title={doc.node.meta_title} keywords={doc.node.meta_keywords} description={doc.node.meta_description} lang='en-gb' />
+      <RenderBody IdCheckPage={doc.node} />
     </Layout>
   )
 }
-export default CredasPage
+export default IdCheckPage
+
