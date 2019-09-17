a710ecf89b1a0a460267041d45de48bdbc4e2799
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-17 16:44:18 +0100

    adding paste bin

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -3,33 +3,20 @@ const fetch = require('node-fetch')
 const { CREDAS_TOKEN } = process.env
 
 exports.handler = async (event, context, callback) => {
-  let payload = JSON.parse(event.body).payload;
-  console.log("START SENDING")
-  return fetch('https://postb.in/b/1568734216551-1112770559266', {
-    method: 'POST',
-    headers: {
-      apikey: CREDAS_TOKEN,
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({
-      forename: payload.data.forename,
-      surname: payload.data.surname,
-      phoneNumber: payload.data.phoneNumber,
-      emailAddress: payload.data.emailAddress,
-      regTypeId: "Cc466776a-9663-4812-8312-c5f00aa90796",
-      referenceId: "HID/12345/336/10",
-      sendEmail: true,
-      diallingCode: "+44",
-      duplicateAcknowledgement: false,
-      provideWebJourneyLink: true,
+  let payload = JSON.parse(event.body).payload
+
+  try {
+    console.log("START SENDING")
+    return fetch('https://postb.in/b/1568734216551-1112770559266', {
+      method: 'POST',
+      headers: {
+        apikey: CREDAS_TOKEN,
+        'Content-Type': 'application/json'
+      },
+      body: 'HELLO'
     })
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log("PAYLOAD SENT")
-  })
-  .catch(error => (
-      console.log("THIS IS CRAP")
-    )
-  )
+  } catch( err ) {
+    console.log('ERRORED')
+    console.log(err)
+  }
 }
