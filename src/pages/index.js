a0cdc38803f1c48fcabfec025a968ae2542efc6e
Jodie Doubleday <jodie.doubleday@gmail.com>
2019-09-24 22:06:50 +0100

    remove cloudinary

--- src/pages/index.js
+++ src/pages/index.js
@@ -126,7 +126,7 @@ const RenderBody = ({ homePage, bidJs }) => {
                     <div className='col-xs-6 col-sm-4 col-lg-3 d-flex flex-column'  key={`item-${index}`}>
                       <a className='thumbnail pos-rel flex-grow d-flex flex-column' href={`/auction/#!/itemDetails/340/${item.node.itemid}`}>
                         <span className='label label-primary pos-abs'>Lot {item.node.lotNumber}</span>
-                        <Cloudinary url={item.node.imageUrls[0]} />
+                        {/* <Cloudinary url={item.node.imageUrls[0]} /> */}
                         <div className='caption mt-auto'>
                           <small className='text-muted'>Ends: {endDate}</small>
                           <h3 className='h5 mb-0 text-muted' dangerouslySetInnerHTML={{__html: item.node.title}} />
